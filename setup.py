a1870617993c74206f2d7e51286976bc1d3d327f
Alex <alxgrmv@gmail.com>
2015-07-12 10:46:23 +0600

    + setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+from userflow import __version__
+
+
+setup(
+    name='userflow',
+    version='.'.join(map(str, __version__)),
+    description='Another users django app',
+    url='https://github.com/alexey-grom/django-userflow',
+    author='alxgrmv@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=(
+        'django',
+    ),
+)
--- userflow/__init__.py
+++ userflow/__init__.py
@@ -1,3 +1,5 @@
 # encoding: utf-8
 
+__version__ = 1, 0, 0
+
 default_app_config = 'userflow.apps.UsersConfig'
